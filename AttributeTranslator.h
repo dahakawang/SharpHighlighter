bd4d0a421658bb08aecebb449022b9965bd94f19
David <DevilDavidWang@gmail.com>
2015-01-11 19:16:16 +0800

    add ability to translate a color and background property in the theme file

--- AttributeTranslator.h
+++ AttributeTranslator.h
@@ -9,5 +9,29 @@
 #import <Foundation/Foundation.h>
 
 @interface AttributeTranslator : NSObject
++ (AttributeTranslator*)sharedTranslator;
+- (NSDictionary*)getStyleFromDefinition: (NSDictionary*)definition;
+@end
+
+@interface Translator : NSObject
+- (NSString*)getName;
+- (void)addDefinition: (NSString*)definition ToStyle: (NSMutableDictionary*)style;
+- (id)translateColorFromString: (NSString*)def;
+@end
+
+
+@interface ColorTranslator : Translator
+
+@end
+
+@interface BackgroundTranslator : Translator
+
+@end
+
+@interface FontWeightTranslator : Translator
+
+@end
+
+@interface FontStyleTranslator : Translator
 
 @end
--- AttributeTranslator.m
+++ AttributeTranslator.m
@@ -6,8 +6,112 @@
 //  Copyright (c) 2015 maxtrixcube. All rights reserved.
 //
 
+#import <Cocoa/Cocoa.h>
 #import "AttributeTranslator.h"
 
-@implementation AttributeTranslator
+@implementation AttributeTranslator {
+  NSMutableDictionary* property;
+}
 
++ (AttributeTranslator*)sharedTranslator {
+  static AttributeTranslator* instance = nil;
+  if (!instance) instance = [[AttributeTranslator alloc] init];
+  return instance;
+}
+
+- (NSDictionary*)getStyleFromDefinition: (NSDictionary*)definition {
+  NSMutableDictionary* style = [[NSMutableDictionary alloc] init];
+  
+  for (NSString* name in definition) {
+    if (property[name]) {
+      [property[name] addDefinition:definition[name] ToStyle:style];
+    } else {
+      NSLog(@"property %@ not supported", name);
+    }
+  }
+  
+  return style;
+}
+
+- (instancetype)init {
+  if (self = [super init]) {
+    [self setup];
+  }
+  return self;
+}
+
+- (void)setup {
+  NSArray* translatorNames = @[@"ColorTranslator", @"BackgroundTranslator"];
+  property = [[NSMutableDictionary alloc] init];
+  
+  for (NSString* className in translatorNames) {
+    [self registerClassByName:className];
+  }
+  
+}
+
+- (void)registerClassByName: (NSString*)className {
+  Class clazz = NSClassFromString(className);
+  Translator* translator = [[clazz alloc] init];
+  property[[translator getName]] = translator;
+}
+@end
+
+
+
+
+@implementation Translator
+- (NSString*)getName {
+  @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"Not implemented" userInfo:NULL];
+}
+
+- (void)addDefinition: (NSString*)definition ToStyle: (NSMutableDictionary*)style {
+  @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"Not implemented" userInfo:NULL];
+}
+
+- (float)parseHexFromString: (NSString*)string {
+  NSScanner *scanner = [NSScanner scannerWithString:string];
+  
+  unsigned int result;
+  [scanner scanHexInt:&result];
+  return result;
+}
+
+- (id)translateColorFromString: (NSString*)def {
+  float r, g, b;
+  NSString* definition = [def lowercaseString];
+  r = [self parseHexFromString:[definition substringWithRange:NSMakeRange(1, 2)]];
+  g = [self parseHexFromString:[definition substringWithRange:NSMakeRange(3, 2)]];
+  b = [self parseHexFromString:[definition substringWithRange:NSMakeRange(5, 2)]];
+  
+#if TARGET_OS_MAC
+  NSColor* color =  [NSColor colorWithRed:r green:g blue:b alpha:1];
+  return color;
+#elif TARGET_OS_IPHONE
+  //TODO add ios implementation
+  @throw [NSException exceptionWithName:NSInvalidArgumentException reason:@"Not implemented" userInfo:NULL];
+#endif
+}
+@end
+
+
+@implementation ColorTranslator
+- (NSString*)getName {
+  return @"color";
+}
+
+- (void)addDefinition: (NSString*)definition ToStyle: (NSMutableDictionary*)style {
+  style[NSForegroundColorAttributeName] = [self translateColorFromString:definition];
+}
+@end
+
+
+@implementation BackgroundTranslator : Translator
+- (NSString*)getName {
+  return @"background";
+}
+
+- (void)addDefinition: (NSString*)definition ToStyle: (NSMutableDictionary*)style {
+  style[NSBackgroundColorAttributeName] = [self translateColorFromString:definition];
+}
 @end
