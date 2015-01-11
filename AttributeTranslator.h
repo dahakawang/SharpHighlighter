c792433294f15568912817abf50794bfc511ead4
David <DevilDavidWang@gmail.com>
2015-01-11 16:43:15 +0800

    add translator to translate a style definition to dictionary attributes for NSAttributedString

---
+++ AttributeTranslator.h
@@ -0,0 +1,13 @@
+//
+//  AttributeTranslator.h
+//  TMHighlighter
+//
+//  Created by david on 1/11/15.
+//  Copyright (c) 2015 maxtrixcube. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AttributeTranslator : NSObject
+
+@end
---
+++ AttributeTranslator.m
@@ -0,0 +1,13 @@
+//
+//  AttributeTranslator.m
+//  TMHighlighter
+//
+//  Created by david on 1/11/15.
+//  Copyright (c) 2015 maxtrixcube. All rights reserved.
+//
+
+#import "AttributeTranslator.h"
+
+@implementation AttributeTranslator
+
+@end
