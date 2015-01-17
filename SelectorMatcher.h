b6319046cbb212ba16c1e38729311d60dd205458
David <DevilDavidWang@gmail.com>
2015-01-17 21:39:48 +0800

    add a css like selector matcher

---
+++ SelectorMatcher.h
@@ -0,0 +1,13 @@
+//
+//  SelectorMatcher.h
+//  TMHighlighter
+//
+//  Created by david on 1/17/15.
+//  Copyright (c) 2015 maxtrixcube. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SelectorMatcher : NSObject
+
+@end
