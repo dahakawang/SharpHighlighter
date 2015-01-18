b5034de06b9f10896f5e8966cc594681c9e32476
David <DevilDavidWang@gmail.com>
2015-01-18 12:29:24 +0800

    add abstract class that compiles a theme

---
+++ renderer/ThemeCompiler.m
@@ -0,0 +1,13 @@
+//
+//  ThemeCompiler.m
+//  TMHighlighter
+//
+//  Created by david on 1/18/15.
+//  Copyright (c) 2015 maxtrixcube. All rights reserved.
+//
+
+#import "ThemeCompiler.h"
+
+@implementation ThemeCompiler
+
+@end
