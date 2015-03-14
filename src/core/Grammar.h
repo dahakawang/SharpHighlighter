6b691c6734d75e1e76921605eadf52f1c450a8e8
David <DevilDavidWang@gmail.com>
2015-03-14 20:35:13 +0800

    add grammar definition class

---
+++ src/core/Grammar.h
@@ -0,0 +1,13 @@
+//
+//  Grammar.h
+//  Pods
+//
+//  Created by david on 3/14/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Grammar : NSObject
+
+@end
---
+++ src/core/Grammar.m
@@ -0,0 +1,13 @@
+//
+//  Grammar.m
+//  Pods
+//
+//  Created by david on 3/14/15.
+//
+//
+
+#import "Grammar.h"
+
+@implementation Grammar
+
+@end
