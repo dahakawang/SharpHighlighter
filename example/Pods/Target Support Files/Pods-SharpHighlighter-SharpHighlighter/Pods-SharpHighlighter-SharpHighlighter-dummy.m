9e321416d6a7ee7a6f4651fa6878cc1b0e697c03
David <DevilDavidWang@gmail.com>
2015-03-24 20:14:53 +0800

    remove all files

--- example/Pods/Target Support Files/Pods-SharpHighlighter-SharpHighlighter/Pods-SharpHighlighter-SharpHighlighter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SharpHighlighter_SharpHighlighter : NSObject
-@end
-@implementation PodsDummy_Pods_SharpHighlighter_SharpHighlighter
-@end
