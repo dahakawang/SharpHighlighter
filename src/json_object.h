3f66a429e0dc4c2f16027432ef2b081829f0afb0
David Wang <DevilDavidWang@gmail.com>
2015-07-16 03:46:12 -0400

    add json object

---
+++ src/json_object.h
@@ -0,0 +1,32 @@
+#ifndef _JSON_OBJECT_
+#define _JSON_OBJECT_
+
+#include <string>
+#include <map>
+
+using std::wstring;
+using std::map;
+
+namespace shl {
+
+class JsonObject {
+public:
+    wstring name;
+    wstring scope_name;
+    vector<wstring> file_types;
+    vector<JsonObject> patterns;
+    map<wstring, JsonObject> repository;
+
+    wstring include;
+    wstring begin;
+    map<wstring, map<wstring, wstring>> begin_captures;
+    wstring end;
+    map<wstring, map<wstring, wstring>> end_captures;
+    wstring content_name;
+    wstring match;
+    map<wstring, map<wstring, wstring>> captures;
+};
+
+}
+
+#endif
