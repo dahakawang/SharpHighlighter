f0953767966764df52eabc6ad69e11c61534d1b2
David Wang <DevilDavidWang@gmail.com>
2015-07-16 03:50:46 -0400

    add grammar

---
+++ src/grammar.h
@@ -0,0 +1,11 @@
+#ifndef _GRAMMAR_H_
+#define _GRAMMAR_H_
+
+namespace shl {
+    class Entry {
+    };
+
+}
+
+
+#endif
---
+++ src/grammar_loader.h
@@ -0,0 +1,11 @@
+#ifndef _GRAMMAR_LOADER_
+#define _GRAMMAR_LOADER_
+
+namespace shl {
+
+class GrammarLoader {
+    
+};
+}
+
+#endif
