59fa28c052d8d0b4ab5c5c7586ec29699e0a1e13
David <DevilDavidWang@gmail.com>
2015-07-18 14:15:36 +0800

    add exception for regex

--- src/shl_exception.h
+++ src/shl_exception.h
@@ -2,7 +2,9 @@
 #define _SHL_EXCEPTION_
 
 #include <stdexcept>
+#include <string>
 
+using std::string;
 using std::runtime_error;
 
 namespace shl {
@@ -16,6 +18,12 @@ public:
 class InvalidGrammarException : public ShlException {
 public:
     explicit InvalidGrammarException(const string& msg):ShlException(msg){};
+};
+
+
+class InvalidRegexException : public ShlException {
+public:
+    explicit InvalidRegexException(const string& msg):ShlException(msg){};
 };
 
 
