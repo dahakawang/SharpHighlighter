90426aea640b6bf03d09d6721833d62a4b725d8e
David Wang <DevilDavidWang@gmail.com>
2015-07-20 00:46:12 -0400

    add grammar object

--- src/grammar.h
+++ src/grammar.h
@@ -1,10 +1,37 @@
 #ifndef _GRAMMAR_H_
 #define _GRAMMAR_H_
 
-namespace shl {
-    class Entry {
-    };
+#include <string>
+#include <vector>
+#include <map>
+#include "regex.h"
 
+using std::string;
+using std::map;
+using std::vector;
+
+namespace shl {
+
+class Pattern {
+public:
+    bool has_end;
+    string name;
+    Regex begin;
+    map<int, string> captures;
+    Regex end;
+    map<int, string> begin_captures;
+    map<int, string> end_captures;
+    vector<Pattern> patterns;
+    string content_name;
+    Pattern* include; // like a kind of weak pointer, only points to entries in repository
+};
+
+class Grammar : public Pattern {
+public:
+    string desc;
+    vector<string> file_types;
+    map<string, Pattern> repository;
+};
 }
 
 
