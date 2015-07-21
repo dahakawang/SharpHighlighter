4114f1638a97c478e5450d1fa79dcaa77bc881ed
David Wang <DevilDavidWang@gmail.com>
2015-07-21 03:56:56 -0400

    add next_lexeme implementation

--- src/tokenizer.cc
+++ src/tokenizer.cc
@@ -11,47 +11,63 @@ vector<pair<Range, Scope> > Tokenizer::tokenize(const Grammar& grammar, const st
     stack<const Pattern*> pattern_stack;
 
     tokens.push_back(std::make_pair(Range(0, text.size()), Scope(grammar.name)));
-    tokenize(text, grammar, Match(), pattern_stack, tokens);
+    tokenize(text, grammar, Match::make_dummy(0,0), pattern_stack, tokens);
 
     return tokens;
 }
 
-bool Tokenizer::next_lexeme(const string& text, const Match& begin_lexeme, const Pattern& pattern, const Pattern* parent, const Pattern** found, Match& match) {
-    int pos = (begin_lexeme == Match::NOT_MATCHED) ? 0 : begin_lexeme[0].end();
-    Match first_match;
+/**
+ * Find the next lexeme iteratively
+ *
+ * This method find the next lexeme by matching all begin field of its sub-patterns
+ * as well as its own end field, whichever comes first will be seleteced
+ *
+ * When true returned, found will contain the selected next pattern, and match will hold 
+ * the results. When false returned, there are 3 scenarios:
+ * 1. end field is matched, found & match contains results
+ * 2. current pattern is toplevel rule, so no end field, found is nullptr
+ * 3. When nothing can be matched(either source text or syntax definition is invalid),
+ *    and user specified OPTION_TOLERATE_ERROR, found will be nullptr, otherwise exception
+ *    will be thrown
+ */
+bool Tokenizer::next_lexeme(const string& text, const Match& begin_lexeme, const Pattern& pattern, const Pattern** found, Match& match) {
+    int pos = begin_lexeme[0].end();
     const Pattern* found_pattern;
+    Match first_match;
     bool is_close = false;
 
+    // first find pattern or end pattern, whichever comes first
     for (const Pattern& sub_pattern : pattern.patterns) {
         Match tmp = sub_pattern.begin.match(text, pos);
         if (tmp != Match::NOT_MATCHED) {
-            if( first_match == Match::NOT_MATCHED || tmp[0].position < first_match[0].position) {
-                first_match = std::move(tmp); //! tmp never used afterwards
+            if( found_pattern == nullptr || tmp[0].position < first_match[0].position) {
+                first_match = std::move(tmp); 
                 found_pattern = &sub_pattern;
             }
         }
     }
     if (!pattern.end.empty()) {
         Match tmp = pattern.end.match(text, pos);
-        if( first_match == Match::NOT_MATCHED || tmp[0].position < first_match[0].position) {
-            first_match = std::move(tmp); //! tmp never used afterwards
+        if( found_pattern != nullptr || tmp[0].position < first_match[0].position) {
+            first_match = std::move(tmp); 
             found_pattern = &pattern;
             is_close = true;
         }
-    } else {
-        if (found_pattern == nullptr) {
-            is_close = true;
-
-        }
     }
-
     if ( found_pattern != nullptr) {
         *found = found_pattern;
         match = first_match;
         return is_close;
     }
 
-
+    // special handle for toplevel rule
+    if (pattern.end.empty()) {
+        // all rule with begin will has an end, which is enforced by grammar loader
+        // so only toplevel rules can be here
+        *found = nullptr;
+        is_close = true;
+        return false;
+    }
 
     // When no lexeme found
     if (_option & OPTION_TOLERATE_ERROR == 0) {
@@ -63,12 +79,11 @@ bool Tokenizer::next_lexeme(const string& text, const Match& begin_lexeme, const
 }
 
 void Tokenizer::tokenize(const string& text, const Pattern& pattern, const Match& begin_lexeme, stack<const Pattern*>& stack, vector<pair<Range, Scope> >& tokens) {
-    const Pattern* parent = stack.empty() ? nullptr : stack.top();
     stack.push(&pattern);
 
     const Pattern* found_pattern;
     Match match;
-    while(next_lexeme(text, begin_lexeme, pattern, parent, &found_pattern, match)) {
+    while(next_lexeme(text, begin_lexeme, pattern, &found_pattern, match)) {
         
     }
 
