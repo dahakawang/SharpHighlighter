0834b806b25e75acc36d3c91df36c3ee49528202
David <DevilDavidWang@gmail.com>
2015-07-26 10:54:38 +0800

    to support applyEndPatternLast and perl \G

--- src/tokenizer.cc
+++ src/tokenizer.cc
@@ -43,6 +43,14 @@ void for_all_subrules(const vector<Pattern>& patterns, function<void(const Patte
     for_all_subrules(patterns, callback, 2);
 }
 
+inline bool is_end_match_first(const Pattern& pattern, const Match& end_match, const Match& current_first_match) {
+    if (pattern.applyEndPatternLast) {
+        return end_match[0].position < current_first_match[0].position;
+    } else {
+        return end_match[0].position <= current_first_match[0].position;
+    }
+}
+
 /**
  * Find the next lexeme iteratively
  *
@@ -56,16 +64,20 @@ void for_all_subrules(const vector<Pattern>& patterns, function<void(const Patte
  * 3. When nothing can be matched(either source text or syntax definition is invalid),
  *    and user specified OPTION_TOLERATE_ERROR, found will be nullptr, otherwise exception
  *    will be thrown
+ *
+ * Note:
+ * begin_end_pos: this is the offset of current pattern's begin match.end(), it's meant to 
+ * support Perl style \G, and stands for the *previous* match.end()
  */
-bool Tokenizer::next_lexeme(const string& text, const Match& begin_lexeme, const Pattern& pattern, const Pattern** found, Match& match) {
+bool Tokenizer::next_lexeme(const string& text, const int begin_end_pos, const Match& begin_lexeme, const Pattern& pattern, const Pattern** found, Match& match) {
     int pos = begin_lexeme[0].end();
     const Pattern* found_pattern = nullptr;
     Match first_match;
     bool is_close = false;
 
     // first find pattern or end pattern, whichever comes first
-    for_all_subrules(pattern.patterns, [&found_pattern, &first_match, pos, &text](const Pattern& sub_pattern) {
-        Match tmp = sub_pattern.begin.match(text, pos);
+    for_all_subrules(pattern.patterns, [&found_pattern, &first_match, pos, &text, begin_end_pos](const Pattern& sub_pattern) {
+        Match tmp = sub_pattern.begin.match(text, pos, begin_end_pos);
         if (tmp != Match::NOT_MATCHED) {
             if( found_pattern == nullptr || tmp[0].position < first_match[0].position) {
                 first_match = std::move(tmp); 
@@ -74,10 +86,10 @@ bool Tokenizer::next_lexeme(const string& text, const Match& begin_lexeme, const
         }
     });
     if (!pattern.end.empty()) {
-        Match tmp = pattern.end.match(text, pos);
-        if (tmp != Match::NOT_MATCHED) {
-            if( found_pattern == nullptr || tmp[0].position < first_match[0].position) {
-                first_match = std::move(tmp); 
+        Match end_match = pattern.end.match(text, pos, begin_end_pos);
+        if (end_match != Match::NOT_MATCHED) {
+            if( found_pattern == nullptr || is_end_match_first(pattern, end_match, first_match)) {
+                first_match = std::move(end_match); 
                 found_pattern = &pattern;
                 is_close = true;
             }
@@ -120,6 +132,7 @@ vector<string> get_name(vector<const Pattern*>& stack, const string& name) {
 
 void Tokenizer::add_scope(vector<pair<Range, Scope> >& tokens, const Range& range, vector<const Pattern*>& stack, const string& name) {
     if (name.empty()) return;
+    if (range.length == 0) return; // only captures can potentially has 0 length
 
     Scope scope(get_name(stack, name));
     tokens.push_back(std::make_pair(range, scope));
@@ -146,10 +159,11 @@ void Tokenizer::process_capture(vector<pair<Range, Scope> >& tokens, const Match
 Match Tokenizer::tokenize(const string& text, const Pattern& pattern, const Match& begin_lexeme, vector<const Pattern*>& stack, vector<pair<Range, Scope> >& tokens) {
     stack.push_back(&pattern);
 
+    int begin_end_pos = begin_lexeme[0].end();
     const Pattern* found_pattern = nullptr;
     Match last_lexeme, match;
     last_lexeme = begin_lexeme;
-    while(next_lexeme(text, last_lexeme, pattern, &found_pattern, match)) {
+    while(next_lexeme(text, begin_end_pos, last_lexeme, pattern, &found_pattern, match)) {
         if (found_pattern->is_match_rule) {
             add_scope(tokens, match[0], stack, found_pattern->name);
             process_capture(tokens, match, stack, found_pattern->captures);
