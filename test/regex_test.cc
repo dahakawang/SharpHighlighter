622079f165aa9bb7868161e77eeea81b368a8511
David Wang <DevilDavidWang@gmail.com>
2015-08-10 05:56:24 -0400

    first test

--- test/regex_test.cc
+++ test/regex_test.cc
@@ -1,12 +1,19 @@
 //#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include <regex.h>
+#include <iostream>
 
 using namespace shl;
+using namespace std;
 
 TEST_CASE("Regex can find matchs") {
     SECTION("find a simple match") {
         Regex r("\\d{3}-\\d{4}");
         auto result = r.match("hello my Tel. is 322-0592", 0);
+        if (result == Match::NOT_MATCHED) {
+            cout << "not matched" << endl;
+        }else {
+            cout << "matched" << endl;
+        }
     }
 }
