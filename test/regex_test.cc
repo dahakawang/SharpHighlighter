454f9e1c3b1a2ca4cd63fe2d26a6c8e188688e58
David Wang <DevilDavidWang@gmail.com>
2015-08-10 05:03:59 -0400

    add a regex test

---
+++ test/regex_test.cc
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+#include <regex.h>
+
+using namespace shl;
+
+TEST_CASE("Regex can find matchs") {
+    SECTION("find a simple match") {
+        Regex r("\\d{3}-\\d{4}");
+        auto result = r.match("hello my Tel. is 322-0592", 0);
+    }
+}
