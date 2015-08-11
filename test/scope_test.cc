b3c243c118ee2b99c9f4357d4d70fdb813b28a39
David Wang <DevilDavidWang@gmail.com>
2015-08-11 05:59:56 -0400

    add test for Scope

--- test/scope_test.cc
+++ test/scope_test.cc
@@ -30,4 +30,54 @@ TEST_CASE("Scope Name Test") {
         REQUIRE(name.name() == "");
         REQUIRE(name.breakdown().size() == 0);
     }
+
+    SECTION("scope can be compared") {
+        ScopeName name1("source.control.c.if");
+        ScopeName name2("haha");
+        ScopeName name3 = name1;
+
+        REQUIRE(name1 != name2);
+        REQUIRE_FALSE(name1 == name2);
+        REQUIRE(name1 == name3);
+    }
+}
+
+
+TEST_CASE("Scope Test") {
+    
+    SECTION("scope be constructed from string") {
+        Scope s("   source    punctuation.definition.string.end.c        variable.parameter.preprocessor.c  ");
+
+        REQUIRE(s.size() == 3);
+        REQUIRE(s[0].name() == "source");
+        REQUIRE(s[1].name() == "punctuation.definition.string.end.c");
+        REQUIRE(s[2].name() == "variable.parameter.preprocessor.c");
+    }
+
+    SECTION("scope name is normalized") {
+        Scope s("   source    punctuation.definition.string.end.c        variable.parameter.preprocessor.c  ");
+
+        REQUIRE(s.name() == "source punctuation.definition.string.end.c variable.parameter.preprocessor.c");
+    }
+
+    SECTION("scope will be broken up into ScopeNames") {
+        Scope s("source punctuation.definition.string.end.c variable.parameter.preprocessor.c");
+
+        REQUIRE(s.size() == 3);
+        REQUIRE(s[0].name() == "source");
+        REQUIRE(s[1].name() == "punctuation.definition.string.end.c");
+    }
+
+    SECTION("scope can be compared") {
+        Scope s1("source punctuation.definition.string.end.c variable.parameter.preprocessor.c");
+        Scope s2 = s1;
+        Scope s3("ource punctuation.definition.string.end.c variable.parameter.preprocessor.c");
+
+        REQUIRE(s1 == s2);
+        REQUIRE(s1 != s3);
+        REQUIRE_FALSE(s1 != s2);
+        REQUIRE_FALSE(s1 == s3);
+
+    }
+
 }
