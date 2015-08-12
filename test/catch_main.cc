cba41b189645a101fb75cada4b2bde8418e8b604
David Wang <DevilDavidWang@gmail.com>
2015-08-12 05:49:51 -0400

    add helper method to load all data from file

--- test/catch_main.cc
+++ test/catch_main.cc
@@ -1,2 +1,30 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include <sstream>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+#include <stdexcept>
+
+using std::ostringstream;
+using std::string;
+using std::ifstream;
+using std::copy;
+using std::istreambuf_iterator;
+using std::ostreambuf_iterator;
+
+
+const string load_string(const string& file_path) {
+    string path = "test/" + file_path;
+    ostringstream stream;
+    ifstream fs(path);
+
+    if (!fs.good()) {
+        throw std::runtime_error("error access file " + path);
+    }
+
+    copy(istreambuf_iterator<char>(fs), istreambuf_iterator<char>(), ostreambuf_iterator<char>(stream));
+    
+    return stream.str();
+}
---
+++ test/util.h
@@ -0,0 +1,10 @@
+#ifndef __UTIL_H__
+#define __UTIL_H__
+
+#include <string>
+
+using std::string;
+
+const string load_string(const string& file_path);
+
+#endif
