086cdc9077544db25c470af87002243071a492bb
David Wang <DevilDavidWang@gmail.com>
2015-08-13 02:55:52 -0400

    add a test for tokenizer

--- test/tokenizer_test.cc
+++ test/tokenizer_test.cc
@@ -17,15 +17,26 @@ TEST_CASE("Tokenizer Tests") {
 
         REQUIRE( tokens.size() == 4 );
         REQUIRE( tokens[0].first.substr(source) == source );
-        REQUIRE( tokens[0].second.name() == "source.hello");
+        REQUIRE( tokens[0].second.name() == "source.hello" );
 
         REQUIRE( tokens[1].first.substr(source) == "hello" );
-        REQUIRE( tokens[1].second.name() == "source.hello prefix.hello");
+        REQUIRE( tokens[1].second.name() == "source.hello prefix.hello" );
 
         REQUIRE( tokens[2].first.substr(source) == "world!" );
-        REQUIRE( tokens[2].second.name() == "source.hello suffix.hello");
+        REQUIRE( tokens[2].second.name() == "source.hello suffix.hello" );
 
         REQUIRE( tokens[3].first.substr(source) == "!" );
-        REQUIRE( tokens[3].second.name() == "source.hello suffix.hello emphasis.hello");
+        REQUIRE( tokens[3].second.name() == "source.hello suffix.hello emphasis.hello" );
+    }
+
+    SECTION("return a single token when matches a single pattern with no capture groups") {
+        string data = load_string("fixture/coffee-script.json");
+        string source = "return";
+        Grammar g = loader.load(data);
+        auto tokens = tokenizer.tokenize(g, source); 
+
+        REQUIRE( tokens.size() == 2 );
+        REQUIRE( tokens[1].first.substr(source) == "return" );
+        REQUIRE( tokens[1].second.name() == "source.coffee keyword.control.coffee" );
     }
 }
