9b9d0e60d17f0fa7e1fd4b885d0cb9db6f7f30b0
David Wang <DevilDavidWang@gmail.com>
2015-08-12 22:26:24 -0400

    add test for JsonLoader

--- test/json_loader_test.cc
+++ test/json_loader_test.cc
@@ -16,5 +16,24 @@ TEST_CASE("JsonLoader Test") {
         REQUIRE(obj.scope_name == "source.c");
         REQUIRE(obj.name == "C");
 
+        REQUIRE(obj.file_types.size() == 2);
+        REQUIRE(obj.file_types[0] == "c");
+        REQUIRE(obj.file_types[1] == "h");
+
+        REQUIRE(obj.patterns.size() == 2);
+        REQUIRE(obj.patterns[0].include == "#preprocessor-rule-enabled");
+        REQUIRE(obj.patterns[1].begin == "\"");
+        REQUIRE(obj.patterns[1].end == "\"");
+        REQUIRE(obj.patterns[1].name == "string.quoted.double.c");
+        REQUIRE(obj.patterns[1].begin_captures.size() == 1);
+        REQUIRE(obj.patterns[1].begin_captures["0"]["name"]  == "punctuation.definition.string.begin.c");
+        REQUIRE(obj.patterns[1].end_captures.size() == 1);
+        REQUIRE(obj.patterns[1].end_captures["0"]["name"]  == "punctuation.definition.string.end.c");
+        REQUIRE(obj.patterns[1].patterns.size() == 3);
+        REQUIRE(obj.patterns[1].patterns[2].include == "$base");
+
+        REQUIRE(obj.repository.size() == 2);
+        REQUIRE(obj.repository["preprocessor-rule-enabled"].begin == "^\\s*(#(if)\\s+(0*1)\\b)");
+        REQUIRE(obj.repository["preprocessor-rule-enabled"].patterns[0].content_name == "comment.block.preprocessor.else-branch");
     }
 }
