a6dc33b3645a6f14060001abf506212165088cd3
David <DevilDavidWang@gmail.com>
2015-08-18 22:00:52 +0800

    add test to check if external grammar include has been included

--- test/grammar_registry_test.cc
+++ test/grammar_registry_test.cc
@@ -43,4 +43,22 @@ TEST_CASE("GrammarRegistry Test") {
         REQUIRE( registry.contain_grammar("text.html.basic") );
         REQUIRE( registry.contain_grammar("source.ruby") );
     }
+
+    SECTION("all include is resolved") {
+        shared_ptr<GrammarSource> source(new TestSource());
+        GrammarRegistry registry(source);
+
+        Grammar* html_grammar_ptr  = &registry.get_grammar("text.html.basic");
+        Grammar& grammar = registry.get_grammar("source.ruby");
+
+        REQUIRE( grammar.patterns.size() == 88 );
+        REQUIRE( grammar.patterns[68].patterns[0].patterns[2].include.name == "source.ruby" );
+        REQUIRE( grammar.patterns[68].patterns[0].patterns[2].include.ptr == &grammar );
+
+        REQUIRE( grammar.patterns[59].patterns[0].patterns[2].include.name == "text.html.basic" );
+        REQUIRE( grammar.patterns[59].patterns[0].patterns[2].include.ptr == html_grammar_ptr );
+
+
+
+    }
 }
