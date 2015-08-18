67de80382d702c2c8971cad559954daae51b675e
David <DevilDavidWang@gmail.com>
2015-08-18 22:10:28 +0800

    add a TestGrammarSource

--- test/catch_main.cc
+++ test/catch_main.cc
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
+#include "util.h"
 #include <sstream>
 #include <iostream>
 #include <string>
@@ -27,4 +28,25 @@ const string load_string(const string& file_path) {
     copy(istreambuf_iterator<char>(fs), istreambuf_iterator<char>(), ostreambuf_iterator<char>(stream));
     
     return stream.str();
+}
+
+TestGrammarSource::TestGrammarSource(const vector<string>& mapping) {
+    if (mapping.size() % 2 != 0) throw std::runtime_error("the mapping should be in pair");
+
+    for(size_t pos = 0; pos < mapping.size(); pos += 2) {
+        _mapping[mapping[pos]] = mapping[pos+1];
+    }
+}
+const std::string TestGrammarSource::load_data(const std::string& grammar_name) const {
+    if (_mapping.count(grammar_name) == 0) return "";
+    return load_string(_mapping.find(grammar_name)->second);
+}
+const std::vector<std::string> TestGrammarSource::list_grammars() const {
+    vector<string> result;
+
+    for ( auto& pair : _mapping ) {
+        result.push_back(pair.first);
+    }
+
+    return result;
 }
--- test/util.h
+++ test/util.h
@@ -2,9 +2,22 @@
 #define __UTIL_H__
 
 #include <string>
+#include <vector>
+#include <grammar_registry.h>
 
 using std::string;
+using std::vector;
 
 const string load_string(const string& file_path);
+
+class TestGrammarSource : public shl::GrammarSource {
+public:
+    TestGrammarSource(const vector<string>& mapping);
+    virtual const std::string load_data(const std::string& grammar_name) const;
+    virtual const std::vector<std::string> list_grammars() const;
+
+private:
+    map<string, string> _mapping;
+};
 
 #endif
