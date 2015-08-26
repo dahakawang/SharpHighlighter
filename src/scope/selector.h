fe4d61a007c2c62300c5507afdcaff8186d43c52
David Wang <DevilDavidWang@gmail.com>
2015-08-26 04:23:01 -0400

    add selector interface

---
+++ src/scope/selector.h
@@ -0,0 +1,31 @@
+#ifndef __SELECTOR__
+#define __SELECTOR__
+
+namespace shl {
+namespace selector {
+
+class Selector {
+};
+
+class CompositeSelctor {
+};
+
+class ExpressionSelector {
+};
+
+class FilterSelector {
+};
+
+class GroupSelector {
+};
+
+class ScopeSelector {
+};
+
+class ScopeNameSelector {
+};
+
+}
+}
+
+#endif
