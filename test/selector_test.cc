6db83e5b96af193c0a02d04438fbf68b7eb35ca6
David <DevilDavidWang@gmail.com>
2015-09-28 20:37:04 +0800

    add test

--- test/selector_test.cc
+++ test/selector_test.cc
@@ -1,9 +1,14 @@
 #include "catch.hpp"
 #include "util.h"
 #include <iostream>
+#include <selector_parser.h>
 
 using namespace shl;
 
-TEST_CASE("Selector Test") {
+TEST_CASE("Selector Parser Test") {
+    SECTION("parse a simple selector") {
+        selector::Parser parser("source.c storage.modifier.c keyword.core.control.if highlight.*");
 
+        parser.parse_selector();
+    }
 }
