a32f220368ce2dcde31afaec36c61a473fe46605
David Wang <DevilDavidWang@gmail.com>
2015-10-08 05:50:19 -0400

    add support for L&R&B when matching scopes

--- src/scope/selector.h
+++ src/scope/selector.h
@@ -12,13 +12,13 @@ namespace shl {
 namespace selector {
 
 struct AbstractSelector {
-    virtual bool match(const Scope& scope, double* rank) const = 0;
+    virtual bool match(const Scope& scope, char side, double* rank) const = 0;
 };
 
 
 struct ScopeNameSelector : public AbstractSelector {
     ScopeNameSelector(): anchor_prev(false) {};
-    virtual bool match(const Scope& scope, double* rank) const;
+    virtual bool match(const Scope& scope, char side, double* rank) const;
 
     bool anchor_prev;
     vector<string> components;
@@ -26,7 +26,7 @@ struct ScopeNameSelector : public AbstractSelector {
 
 struct ScopeSelector : public AbstractSelector {
     ScopeSelector(): anchor_begin(false), anchor_end(false) {};
-    virtual bool match(const Scope& scope, double* rank) const;
+    virtual bool match(const Scope& scope, char side, double* rank) const;
 
     vector<ScopeNameSelector> atoms;
     bool anchor_begin, anchor_end;
@@ -37,7 +37,7 @@ struct FilterSelector : public AbstractSelector {
     enum Side { Left = 'L', Right = 'R', Both = 'B' };
 
     FilterSelector(): side(Left) {};
-    virtual bool match(const Scope& scope, double* rank) const;
+    virtual bool match(const Scope& scope, char side, double* rank) const;
 
     unique_ptr<AbstractSelector> selector;
     Side side;
@@ -45,7 +45,7 @@ struct FilterSelector : public AbstractSelector {
 
 struct ExpressionSelector : public AbstractSelector {
     ExpressionSelector(): is_negative(false) {};
-    virtual bool match(const Scope& scope, double* rank) const;
+    virtual bool match(const Scope& scope, char side, double* rank) const;
 
     unique_ptr<AbstractSelector> selector;
     bool is_negative;
@@ -54,20 +54,20 @@ struct ExpressionSelector : public AbstractSelector {
 struct CompositeSelctor : public AbstractSelector {
     enum CompositionType { OR = '|', AND = '&', MINUS = '-', NONE = 0 };
 
-    virtual bool match(const Scope& scope, double* rank) const;
+    virtual bool match(const Scope& scope, char side, double* rank) const;
 
     vector<ExpressionSelector> selectors;
     vector<CompositionType> operators;
 };
 
 struct Selector : public AbstractSelector {
-    virtual bool match(const Scope& scope, double* rank) const;
+    virtual bool match(const Scope& scope, char side, double* rank) const;
 
     vector<CompositeSelctor> selectors;
 };
 
 struct GroupSelector : public AbstractSelector  {
-    virtual bool match(const Scope& scope, double* rank) const;
+    virtual bool match(const Scope& scope, char side, double* rank) const;
 
     Selector selector;
 };
