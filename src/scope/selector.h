f374796348638a55c27c20cc70ab942c38ce0c64
David <DevilDavidWang@gmail.com>
2015-10-19 20:52:15 +0800

    add str() interface

--- src/scope/selector.h
+++ src/scope/selector.h
@@ -13,12 +13,14 @@ namespace selector {
 
 struct AbstractSelector {
     virtual bool match(const Scope& scope, char side, double* rank) const = 0;
+    virtual string str() const = 0;
 };
 
 
 struct ScopeNameSelector : public AbstractSelector {
     ScopeNameSelector(): anchor_prev(false) {};
     virtual bool match(const Scope& scope, char side, double* rank) const;
+    virtual string str() const;
 
     bool anchor_prev;
     vector<string> components;
@@ -27,6 +29,7 @@ struct ScopeNameSelector : public AbstractSelector {
 struct ScopeSelector : public AbstractSelector {
     ScopeSelector(): anchor_begin(false), anchor_end(false) {};
     virtual bool match(const Scope& scope, char side, double* rank) const;
+    virtual string str() const;
 
     vector<ScopeNameSelector> atoms;
     bool anchor_begin, anchor_end;
@@ -38,6 +41,7 @@ struct FilterSelector : public AbstractSelector {
 
     FilterSelector(): side(Left) {};
     virtual bool match(const Scope& scope, char side, double* rank) const;
+    virtual string str() const;
 
     std::shared_ptr<AbstractSelector> selector;
     Side side;
@@ -46,6 +50,7 @@ struct FilterSelector : public AbstractSelector {
 struct ExpressionSelector : public AbstractSelector {
     ExpressionSelector(): is_negative(false) {};
     virtual bool match(const Scope& scope, char side, double* rank) const;
+    virtual string str() const;
 
     FilterSelector selector;
     bool is_negative;
@@ -55,6 +60,7 @@ struct CompositeSelctor : public AbstractSelector {
     enum CompositionType { OR = '|', AND = '&', MINUS = '-', NONE = 0 };
 
     virtual bool match(const Scope& scope, char side, double* rank) const;
+    virtual string str() const;
 
     vector<ExpressionSelector> selectors;
     vector<CompositionType> operators;
@@ -62,12 +68,14 @@ struct CompositeSelctor : public AbstractSelector {
 
 struct Selector : public AbstractSelector {
     virtual bool match(const Scope& scope, char side, double* rank) const;
+    virtual string str() const;
 
     vector<CompositeSelctor> selectors;
 };
 
 struct GroupSelector : public AbstractSelector  {
     virtual bool match(const Scope& scope, char side, double* rank) const;
+    virtual string str() const;
 
     Selector selector;
 };
