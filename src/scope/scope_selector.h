6233ebb88ac163d004059a25fdd7ea4b0e7ee4ca
David <DevilDavidWang@gmail.com>
2015-10-19 20:52:58 +0800

    add interface to convert selector to string

--- src/scope/scope_selector.h
+++ src/scope/scope_selector.h
@@ -13,6 +13,7 @@ public:
     Selector(const string& selector);
     bool match(const Scope& scope, Side side = BOTH, double* rank = nullptr) const;
     bool match(const string& scope, Side side = BOTH, double* rank = nullptr) const;
+    string str() const { return selector.str(); };
 
 private:
     selector::Selector selector;
