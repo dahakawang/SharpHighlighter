682effedd298b2d7761b43ada1b6b2102cc9e816
David Wang <DevilDavidWang@gmail.com>
2015-10-19 02:39:04 -0400

    add test for php language

--- test/grammar_registry_test.cc
+++ test/grammar_registry_test.cc
@@ -13,22 +13,25 @@ public:
 private:
     string ruby;
     string html;
+    string php;
 };
 
 TestSource::TestSource() {
     ruby = load_string("fixture/ruby.json");
     html = load_string("fixture/html.json");
+    php = load_string("fixture/php.json");
 }
 
 const std::string TestSource::load_data(const std::string& grammar_name) const {
     if ( grammar_name == "text.html.basic" ) return html;
     if ( grammar_name == "source.ruby" ) return ruby;
+    if ( grammar_name == "text.html.php" ) return php;
 
     return "";
 }
 
 const std::vector<std::string> TestSource::list_grammars() const {
-    vector<string> list = {"text.html.basic", "source.ruby"};
+    vector<string> list = {"text.html.basic", "source.ruby", "text.html.php"};
 
     return list;
 }
@@ -57,8 +60,14 @@ TEST_CASE("GrammarRegistry Test") {
 
         REQUIRE( grammar.patterns[59].patterns[0].patterns[2].include.name == "text.html.basic" );
         REQUIRE( grammar.patterns[59].patterns[0].patterns[2].include.ptr == html_grammar_ptr );
+    }
 
+    SECTION("all injections is loaded") {
+        shared_ptr<GrammarSource> source(new TestSource());
+        GrammarRegistry registry(source);
 
-
+        Grammar& php = registry.get_grammar("text.html.php");
+        REQUIRE( !php.empty() );
+        REQUIRE( !php.injections.empty() );
     }
 }
